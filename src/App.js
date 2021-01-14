08d4e3f2411940a7abddd5c0ff22eb1946b811d1
LuizFelipe <luizfelipers19@gmail.com>
2021-01-14 10:30:18 -0300

    API fetching and response items implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './App.css';
 import SearchInput from './Components/SearchInput/index';
 
 function App() {
 
+
+const apiUrl = 'https://kitsu.io/api/edge/'
+
+
+const [info, setInfo] = useState({
+
+
+});
+
 const [text, setText] = useState('');
+
+useEffect(() => {
+
+  console.log(text);
+  if(text){
+    fetch( `${apiUrl}anime?filter[text]=${text}` )
+    .then((response) => response.json())
+    .then((response) =>{
+      setInfo(response)
+      
+    });
+  }
+}, [text]);
 
 
 
@@ -14,6 +36,18 @@ const [text, setText] = useState('');
       <h1>App AnimeJS</h1>
       <SearchInput value={text} 
       onChange={(str)=> setText(str)}></SearchInput>
+
+    {info.data && (
+      <ul className='animes-list'>
+        {info.data.map((anime)=>(
+          <li key={anime.id}>
+            <img src={anime.attributes.posterImage.small}></img>
+            {anime.attributes.canonicalTitle}
+          </li>
+        ))}
+      </ul>
+    )}
+
     </div>
   );
 }
